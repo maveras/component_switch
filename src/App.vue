c94e870d41d5eacbd18653493600f55abcadee97
Marcelo Vera Salvatierra <marcelo.verasalvatierra@gmail.com>
2017-10-06 18:50:14 -0300

    add base bootstrap dependences

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <template>
-  <div id="app">
-    <h1>sadasax</h1>
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12">sdaas
+      <app-quote>
+      </app-quote>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
+import appQuote from './components/quote.vue'
 
+export default {
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  components: {
+    appQuote
+  }
+}
 </script>
 
 <style>
